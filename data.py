dd7e073ba7dd31b0e274970297431842bc3eb4ac
bmorextreme <vlad.stunt777@gmail.com>
2019-08-16 05:12:25 +0300

    добавил data.py

---
+++ data.py
@@ -0,0 +1,24 @@
+a = "Привет"
+b = "Мир"
+c = 25
+d = "{} {} {} я программист".format(a, b ,c)
+print(d)
+user = "python"
+r = "Привет {name}!".format(name=user)
+
+
+user1 = "python"
+r = f"Привет {user1}!"
+
+
+
+
+
+a = "Привет".upper()
+print(a)
+b = "МИР".lower()
+print(b)
+c = "python".capitalize()
+print(c)
+
+
